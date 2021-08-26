f681ebb7aa6f2a0e55980fd7f1c22396815ee58d
Mauricio Hidalgo <darthsqlus@gmail.com>
2021-08-26 03:51:35 -0700

    Initial commit

---
+++ MLClient/MLClient/default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="MLClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <meta name="viewport" content="width=device-width" />
+        <title>Calling ML</title>
+        <style>
+            .main {
+                text-align:center; 
+            }
+            .GFG {
+                color:#009900;
+                font-size:50px;
+                font-weight:bold;
+            }
+            .geeks {
+                font-style:bold;
+                font-size:20px;
+            }
+        </style>
+        <style type="text/css">
+            body {
+                font-family: Arial;
+                font-size: 10pt;
+            }
+        </style>
+        <style type="text/css">
+            table {
+                border: 1px solid #777;
+                border-collapse: collapse;
+            } 
+
+            table tr th,
+            table tr td {
+                border: 1px solid #777;
+            }
+        </style>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div class="text-center">
+                <h3>Query recommendations by User Id</h3>
+            </div>
+            <table>
+                <tr>
+                    <td>User Id :</td>
+                    <td><asp:TextBox id="txtUserId" runat="server" /></td><td>
+                    </td>
+                    <td>
+                        <asp:LinkButton ID="predictLB" runat="server" OnClick="predictLB_Click1">Predict</asp:LinkButton></td>
+                </tr>
+            </table>
+        <hr />
+        <label name="labelML"></label>
+            <asp:Label ID="Label1" runat="server" Text="Recommendations: "></asp:Label><br />
+            <asp:Label ID="lblResults" runat="server"></asp:Label>
+        </form>
+    </body>
+</html>
---
+++ MLClient/MLClient/default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Text;
+
+
+namespace MLClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string PredictionResult;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void predictLB_Click1(object sender, EventArgs e)
+        {
+            string results;
+            int number = 0;
+            if (txtUserId.Text == "" || (int.TryParse(txtUserId.Text, out number)) == false || Convert.ToInt32(txtUserId.Text) > 50000) 
+            {
+                results = "Only numeric values between 0 and 50000";
+            }
+            else
+            {
+                var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/2ac5a4fe2ef5413a90509bb7b3e1d333/services/2d32880fd88f4554a81e6dbf7ff10d7a/execute?api-version=2.0&details=true");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Authorization", "Bearer NSMFkiaPZOkp/QN8ik0F8WT2gR0jR3sYUUiXr5nZ0qLj6GAmqBzKsAXHVTB/NoLos9dIUiEN41XyZzqhmRM4nw==");
+                request.AddHeader("Content-Type", "application/json");
+                var body = @"{" + "\n" + @"  ""Inputs"": {" + "\n" + @"    ""input1"": {" + "\n" + @"      ""ColumnNames"": [" + "\n" + @"        ""UserId"", " + "\n" + @"        ""Movie Name"", " + "\n" + @"        ""Rating"" " + "\n" + @"      ]," + "\n" +
+                @"      ""Values"": [" + "\n" + @"        [" + "\n" + @"          " + txtUserId.Text + ", " + "\n" + @"          ""value"", " + "\n" + @"          ""0"" " + "\n" + @"        ], " + "\n" + @"        [" + "\n" + @"          ""0"", " + "\n" +
+                @"          ""value""," + "\n" + @"          ""0"" " + "\n" + @"        ]" + "\n" + @"      ]" + "\n" + @"    }" + "\n" + @"  }, " + "\n" + @"  ""GlobalParameters"": {}" + "\n" + @"}";
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                results = convertToTable(response.Content.ToString());
+            }
+            lblResults.Text = results;
+        }
+
+        private string convertToTable (string suggestions)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            dynamic DynamicData = JObject.Parse(suggestions);
+
+            sb.Append("<h3>Recommendations for User: " + DynamicData.Results.output1.value.Values[0][0] + "</h3><br />");
+            sb.Append("<table><th>Title</th>");
+            sb.Append("<tr><td>" + DynamicData.Results.output1.value.Values[0][1] + "</td></tr>");
+            sb.Append("<tr><td>" + DynamicData.Results.output1.value.Values[0][2] + "</td></tr>");
+            sb.Append("<tr><td>" + DynamicData.Results.output1.value.Values[0][3] + "</td></tr>");
+            sb.Append("<tr><td>" + DynamicData.Results.output1.value.Values[0][4] + "</td></tr>");
+            sb.Append("<tr><td>" + DynamicData.Results.output1.value.Values[0][5] + "</td></tr>");
+
+            return sb.ToString();
+        }
+    }
+}
